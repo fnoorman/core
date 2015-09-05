de4c006da10b94a7e3c5894b21b4bb895656e8f6
fnoorman <fahmy@aptinventions.com>
2015-09-05 17:58:48 +0800

    add siwftmailer

--- common/config/main.php
+++ common/config/main.php
@@ -15,6 +15,14 @@ return [
             ],
             'appendTimestamp' => true,
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
 
     ],
 ];
